5f9cc172a4372dbb7253461cdf7243e4b8246975
zikosfc <160150086+zikosfc@users.noreply.github.com>
2024-08-21 20:01:45 -0300

    Update game.js

--- game.js
+++ game.js
@@ -106,10 +106,10 @@ function playerDefend() {
 
 function checkGameOver() {
     if (playerHealth <= 0) {
-        messageBox.textContent = "Você perdeu a batalha!";
+        messageBox.textContent = "Atualiza a pagina até o glorioso ganhar!";
         disableButtons();
     } else if (enemyHealth <= 0) {
-        messageBox.textContent = "Você venceu a batalha!";
+        messageBox.textContent = "O sangue de Jesus tem poder! Deus humilhou o inimigo;
         disableButtons();
     }
 }
